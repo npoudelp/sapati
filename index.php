1fc17db78075d9c0483d3e790442176001a54d03
npoudelp <npoudelp@gmail.com>
2022-02-01 23:10:29 +0545

    First commit

---
+++ index.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>sApati</title>
+    <link rel="stylesheet" href="./css/main.css">
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+    <script src="./js/bootstrap.min.js"></script>
+
+</head>
+
+<body>
+    <!-- navbar starts here -->
+    <div class="nav navbar navbar-expand-lg bg-dark navbar-dark py-3">
+        <div class="container">
+            <a href="#" class="navbar-brand"><span class="text-warning h1 logo">sApati</span></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navlink">X</button>
+            <div class="container navbar-collapse justify-content-center" id="navlink">
+                <ul class="navbar-nav lead">
+                    <li class="nav-item">
+                        <a href="#" class="nav-link active">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Friends</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Account</a>
+                    </li>
+                </ul>
+            </div>
+            <div>
+                <a href="#" class="nav-link"><button class="btn btn-outline-warning">Login</button></a>
+            </div>
+        </div>
+    </div>
+    <!-- navbar ends here -->
+
+    <!-- display board starts here -->
+    <section class="bg-dark text-light p-lg-3 p-5 text-center">
+        <div class="container">
+            <div class="d-sm-flex">
+                <div>
+                    <h1><span class="text-warning">Be your own</span> <span class="text-danger">Accountant</span></h1>
+                    <p class="lead my-4">
+                        Why not manage your every finances with the digital button of your own?<br>
+                        Be smart choose <span class="text-warning">sApati</span>.
+                    </p>
+                    <button class="btn btn-outline-warning">Join Us</button>
+                </div>
+                <img class="img-fluid w-50 d-none d-sm-block" src="./images/showcase.png" alt="">
+            </div>
+        </div>
+    </section>
+    <!-- display board ends here -->
+
+    <!-- mail section starts here -->
+    <section class="bg-warning p-5">
+        <div class="container">
+            <div class="d-md-flex justify-content-between align-items-center">
+                <div>
+                    <span class="text-dark h3 mb-3 mb-md-0">Be helpful to our project, connect with us</span>
+                </div>
+
+                <div class="input-group mail-box">
+                    <input type="text" class="form-control" placeholder="Your Email Address">
+                    <div class="input-group-append">
+                        <button class="btn btn-dark" type="button">Send</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- mail section ends here -->
+
+    <!-- promotion starts here -->
+    <section class="p-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-md">
+                    <div class="card bg-dark text-light">
+                        <div class="card bg-dark">
+                            <div class="card-body text-center">
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+
+</html>
