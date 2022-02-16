2a72c1ae6ad477eaa4d89382872f4a4215d4b9b8
npoudelp <npoudelp@gmail.com>
2022-02-16 21:04:45 +0545

    profile page added

---
+++ pages/profile.php
@@ -0,0 +1,73 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>sApati</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/main.css">
+    <script src="../js/bootstrap.min.js"></script>
+
+</head>
+
+<body>
+    <!-- navbar starts here -->
+    <div class="nav navbar navbar-expand-lg bg-dark navbar-dark py-3">
+        <div class="container">
+            <a href="../index.php" class="navbar-brand"><span class="text-warning h1 logo">sApati</span></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navlink">
+                <i class="bi bi-grid-3x3-gap"></i>
+            </button>
+            <div class="container collapse navbar-collapse justify-content-center" id="navlink">
+                <div class="d-lg-flex">
+                    <div class="container">
+                        <ul class="navbar-nav lead">
+                            <li class="nav-item">
+                                <a href="../index.php" class="nav-link">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="./friends.php" class="nav-link disabled">Friends</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="./accounts.php" class="nav-link disabled">Account</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="container">
+                        <li class="nav-item">
+                            <a href="./register.php" class="btn btn-outline-warning">Sign Up</a>
+                        </li>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- navbar ends here -->
+
+    
+    
+
+    <!-- misc section -->
+    <section class="p-1 bg-warning">
+        <div class="container text-dark text-center">
+            <span class="h1 lead fw-bold text-dark">
+                <?php $year = date("F");
+                $month = date("jS");
+                $day = date("Y");
+                echo $year . " " . $month . " " . $day;
+                ?>
+                <?php $year = date("l");
+                echo $year;
+                ?>
+        </div>
+    </section>
+    <!-- misc ends -->
+
+    <!-- footer starts here -->
+    <?php
+    include_once('../includes/footer.php');                     
+    ?>
+</body>
+
+</html>
