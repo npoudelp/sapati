3d738a82fd7efb371f9f85094c6d644217100620
npoudelp <npoudelp@gmail.com>
2022-04-22 11:56:16 +0545

    tables for subscribed users added

--- sapati.sql
+++ sapati.sql
@@ -31,18 +31,12 @@ CREATE TABLE `accounts` (
   PRIMARY KEY (`aid`),
   KEY `uid` (`uid`),
   CONSTRAINT `accounts_ibfk_1` FOREIGN KEY (`uid`) REFERENCES `users` (`uid`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `accounts`
 --
-
-LOCK TABLES `accounts` WRITE;
-/*!40000 ALTER TABLE `accounts` DISABLE KEYS */;
-INSERT INTO `accounts` VALUES (2,1,'Pawan','Biratnagar','9812345678'),(2,2,'Nima','Kathmandu','9823456789'),(3,3,'Niroj Poudel','Urlabari','9842089124'),(3,4,'Indira Acharya','Tandi','984248090'),(2,5,'Ranjan','Damak','9812423423'),(2,7,'Milan Bhattarai','Miklajung','9876534512'),(4,8,'Sujan Rai','Khotang','9807243344'),(3,9,'Milan Bhattarai','Miklajung 8','098098878'),(3,10,'Milan Bhattarai','Tandi','978732424'),(5,11,'Ushsal Koirala','Biratnagar','9812324324'),(5,12,'Bibek Dahal','Pichara','9877676769');
-/*!40000 ALTER TABLE `accounts` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `balance`
@@ -60,18 +54,30 @@ CREATE TABLE `balance` (
   PRIMARY KEY (`bid`),
   KEY `aid` (`aid`),
   CONSTRAINT `balance_ibfk_1` FOREIGN KEY (`aid`) REFERENCES `accounts` (`aid`)
-) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `balance`
 --
 
-LOCK TABLES `balance` WRITE;
-/*!40000 ALTER TABLE `balance` DISABLE KEYS */;
-INSERT INTO `balance` VALUES (6,1,'1000','Buying cloths','2022-04-07'),(9,8,'50000','Bought freeze and tv','2022-04-02');
-/*!40000 ALTER TABLE `balance` ENABLE KEYS */;
-UNLOCK TABLES;
+--
+-- Table structure for table `connectedEmails`
+--
+
+DROP TABLE IF EXISTS `connectedEmails`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `connectedEmails` (
+  `eid` int(255) NOT NULL AUTO_INCREMENT,
+  `email` varchar(50) NOT NULL,
+  PRIMARY KEY (`eid`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `connectedEmails`
+--
 
 --
 -- Table structure for table `users`
@@ -87,19 +93,12 @@ CREATE TABLE `users` (
   `password` varchar(255) NOT NULL,
   `type` varchar(10) NOT NULL,
   PRIMARY KEY (`uid`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `users`
 --
-
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'admin','admin@sapati.com','$2y$10$NFY9doeV6nEUAUJ6yb.KPeJIr82Mo/0pWRFXPD26FnIlF70lrsNDC','admin'),(2,'users','users@sapati.com','$2y$10$i7YydUGkuw0EEzP8uIh4FOwIlVC/7RFAI2WzNRjyztJUtVm7V2ds2','users'),(3,'Pawan Acharya','pawan@sapati.com','$2y$10$kF.YdAuXl5aeAcxAXmB5de0Yl50nqRdGGpbRPje.KBR/B8SJnydR.','users'),(4,'Ram Kumar','ram@sapati.com','$2y$10$jENIQEl2XBBNU.65URzQjesITN04oQY8HIgW4fBuayYh2anag8az2','users'),(5,'Amit Ghimire','amit@sapati.com','$2y$10$B1y.3Ay1Di3iwshvkZB4Yu6SUo9DsjtxA2U55ldfHMl4qGnqTOLYS','users');
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
@@ -108,5 +107,3 @@ UNLOCK TABLES;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2022-04-17 20:39:57
