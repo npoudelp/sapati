e6e8a4072e7545c094e8b4fa1b7ffa19871337bd
npoudelp <npoudelp@gmail.com>
2022-05-03 14:06:30 +0545

    footer logo updated

--- include/footer.php
+++ include/footer.php
@@ -4,9 +4,9 @@ include_once('./footer.php');
 echo ' 
 <footer class="p-1 bg-dark text-white text-center position-relative">
     <div class="container">
-        <p class="lead">Copyright&copy; <?php echo Date("Y"); ?><strong> <span class="text-warning h4" style="font-family: anand;">pwf/f]</span></p>
+        <p class="lead">Copyright&copy; <?php echo Date("Y"); ?><strong> <span class="text-warning h3 logo""><img src="../images/logo.png" width="auto" height="25px" alt=""></span></p>
         <a href="#navlink" class="position-absolute end-0 bottom-0 p-1 my-1 h1 text-warning">
             <i class="bi bi-arrow-up-circle"></i>
         </a>
     </div>
-</footer>';     ?>
+</footer>';
