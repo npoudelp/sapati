0620177cc9f92f834a65e85ea3eff3d1b92e7e2d
npoudelp <npoudelp@gmail.com>
2022-12-24 10:54:54 +0545

    database updated

--- udharo.sql
+++ udharo.sql
@@ -1,12 +1,27 @@
-CREATE TABLE `users` (
-  `uid` int(255) NOT NULL AUTO_INCREMENT,
-  `name` varchar(25) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `type` varchar(10) NOT NULL,
-  PRIMARY KEY (`uid`)
-);
+-- MariaDB dump 10.19  Distrib 10.9.4-MariaDB, for Linux (x86_64)
+--
+-- Host: localhost    Database: udharo
+-- ------------------------------------------------------
+-- Server version	10.9.4-MariaDB
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `accounts`
+--
+
+DROP TABLE IF EXISTS `accounts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `accounts` (
   `uid` int(255) DEFAULT NULL,
   `aid` int(255) NOT NULL AUTO_INCREMENT,
@@ -16,28 +31,139 @@ CREATE TABLE `accounts` (
   PRIMARY KEY (`aid`),
   KEY `uid` (`uid`),
   CONSTRAINT `accounts_ibfk_1` FOREIGN KEY (`uid`) REFERENCES `users` (`uid`)
-);
+) ENGINE=InnoDB AUTO_INCREMENT=47 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `accounts`
+--
+
+LOCK TABLES `accounts` WRITE;
+/*!40000 ALTER TABLE `accounts` DISABLE KEYS */;
+/*!40000 ALTER TABLE `accounts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `balance`
+--
+
+DROP TABLE IF EXISTS `balance`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `balance` (
   `bid` int(255) NOT NULL AUTO_INCREMENT,
   `aid` int(255) DEFAULT NULL,
   `balance` varchar(255) NOT NULL,
   `comments` varchar(255) DEFAULT NULL,
   `bDate` varchar(15) DEFAULT NULL,
+  `type` varchar(10) NOT NULL,
+  `status` varchar(10) NOT NULL,
   PRIMARY KEY (`bid`),
   KEY `aid` (`aid`),
   CONSTRAINT `balance_ibfk_1` FOREIGN KEY (`aid`) REFERENCES `accounts` (`aid`)
-);
+) ENGINE=InnoDB AUTO_INCREMENT=65 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `balance`
+--
+
+LOCK TABLES `balance` WRITE;
+/*!40000 ALTER TABLE `balance` DISABLE KEYS */;
+/*!40000 ALTER TABLE `balance` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `connectedEmails`
+--
+
+DROP TABLE IF EXISTS `connectedEmails`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `connectedEmails` (
   `eid` int(255) NOT NULL AUTO_INCREMENT,
   `email` varchar(50) NOT NULL,
   PRIMARY KEY (`eid`)
-);
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-create table logs (
-  lid int(255) primary key auto_increment not null,
-  details varchar(255) not null,
-  uid int(255),
-  foreign key (uid) references users(uid)
-);
+--
+-- Dumping data for table `connectedEmails`
+--
+
+LOCK TABLES `connectedEmails` WRITE;
+/*!40000 ALTER TABLE `connectedEmails` DISABLE KEYS */;
+/*!40000 ALTER TABLE `connectedEmails` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `logs`
+--
+
+DROP TABLE IF EXISTS `logs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `logs` (
+  `lid` int(255) NOT NULL AUTO_INCREMENT,
+  `details` varchar(255) NOT NULL,
+  `uid` int(255) DEFAULT NULL,
+  `date` varchar(50) NOT NULL,
+  `time` varchar(50) NOT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`lid`),
+  KEY `uid` (`uid`)
+) ENGINE=MyISAM AUTO_INCREMENT=187 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `logs`
+--
+
+LOCK TABLES `logs` WRITE;
+/*!40000 ALTER TABLE `logs` DISABLE KEYS */;
+INSERT INTO `logs` VALUES
+(183,'Logged in with ip : 103.104.28.177',31,'24-12-2022','05:00:17am','City: Damak, continent: Asia, Country: Nepal, Province: Province 1'),
+(184,'logged out by user no 31',31,'24-12-2022','05:03:40am',NULL),
+(185,'Logged in with ip : 103.104.28.177',31,'24-12-2022','05:05:46am','City: Damak, continent: Asia, Country: Nepal, Province: Province 1'),
+(186,'logged out by user no 31',31,'24-12-2022','05:05:51am',NULL);
+/*!40000 ALTER TABLE `logs` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `uid` int(255) NOT NULL AUTO_INCREMENT,
+  `name` varchar(25) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `type` varchar(10) NOT NULL,
+  PRIMARY KEY (`uid`)
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES
+(31,'admin','admin@admin.com','$2y$10$YbrM8A6SQ9obOD76YXJiO.7cdA93/q7Pwp1Hj.eEwA2K8g8Vi29Z.','admin');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-12-24 10:52:13
